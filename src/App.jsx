4126d005742553643e825829b7ea9eb4054fc59d
69rc <jibrinb271@gmail.com>
2024-06-08 17:21:46 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/tyabo-haulage" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/features" element={<Features />} />
